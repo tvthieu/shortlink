695fe082d6b01b89503277710fc40201f1f8c309
thieu tran <tranthieu8686@gmail.com>
2019-05-06 21:34:54 +0700

    fix cros

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(userAgent.express());
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
     where.is(req.ip, (err, result) => {
         req.geoip = result;
         next();
